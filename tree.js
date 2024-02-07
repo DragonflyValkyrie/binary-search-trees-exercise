d5fe9c2b787fe373f3487893df86bd54b099a37c
DragonflyValkyrie <148598429+DragonflyValkryrie@users.noreply.github.com>
2024-02-07 15:00:19 -0500

    Created a function to create BST and display it

--- tree.js
+++ tree.js
@@ -11,7 +11,22 @@ class Tree {
     this.root = this.buildTree(array);
   }
 
-  buildTree(array) {}
+  buildTree(array) {
+    if (array.length === 0) {
+      return null;
+    }
+
+    const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+
+    const mid = Math.floor(sortedArray.length / 2);
+
+    const node = new Node(sortedArray[mid]);
+
+    node.left = this.buildTree(sortedArray.slice(0, mid));
+    node.right = this.buildTree(sortedArray.slice(mid + 1));
+
+    return node;
+  }
 
   insert(value) {}
 
@@ -34,6 +49,10 @@ class Tree {
   isBalanced() {}
 
   rebalance() {}
+
+  prettyPrint() {
+    prettyPrint(this.root);
+  }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -48,3 +67,8 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 };
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const balancedTree = new Tree(array);
+
+balancedTree.prettyPrint();
